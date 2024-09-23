c367b328320451ad549df8c63fad9ec88f519d87
Sapitash Vlada <vlada.sapitash@gmail.com>
2024-09-23 11:55:50 +0300

    migration compositions TGUC-16

---
+++ database/migrations/2024_09_23_085135_create_compositions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('compositions', function (Blueprint $table) {
+            $table->foreignId('performance_id')->constrained('performances')->onDelete('cascade');
+            $table->foreignId('actor_id')->constrained('actors')->onDelete('cascade');
+            $table->primary(['performance_id', 'actor_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('compositions');
+    }
+};
